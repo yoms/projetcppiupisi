a24a5ae14e782d89896560cf9e28f368cc93553d
campomanes.manuel <campomanes.manuel@1234456a-2cf2-11df-82ad-6129be061994>
2010-03-20 18:57:30 +0000

    Correction probleme de diff

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,10 +1,10 @@
 /********************************************************************************
-** Form generated from reading ui file 'mainwindow.ui'
+** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Sat Mar 20 14:52:15 2010
-**      by: Qt User Interface Compiler version 4.5.2
+** Created: Sat 20. Mar 19:45:24 2010
+**      by: Qt User Interface Compiler version 4.6.2
 **
-** WARNING! All changes made in this file will be lost when recompiling ui file!
+** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
 
 #ifndef UI_MAINWINDOW_H
@@ -82,7 +82,6 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0, QApplication::UnicodeUTF8));
         pushButton->setText(QApplication::translate("MainWindow", "Next", 0, QApplication::UnicodeUTF8));
-        Q_UNUSED(MainWindow);
     } // retranslateUi
 
 };
